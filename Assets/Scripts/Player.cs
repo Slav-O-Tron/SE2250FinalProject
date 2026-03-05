dcccc2d1c7cfce93813cfd61053a276cf78ff162
Shawn Gomez <sgomez6@uwo.ca>
2026-03-04 19:38:50 -0500

    Trying to get player movement and camera Test 2

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,8 +3,15 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float moveSpeed = 4f;
+    public float sprintSpeed = 8f;
+    public float gravity = 9.81f;
+    public float jumpHeight = 10; 
+    
+    
     CharacterController controller;
     private Vector3 moveInput;
+    private float verticalVelocity;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -15,10 +22,33 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        InputManagement();
+    }
+
+    private void InputManagement()
+    {
         moveInput.x = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
         moveInput.z = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
-             
+        moveInput.y = VerticalForceCalculation();
         controller.Move(moveInput);
-     
     }
+    
+    private float VerticalForceCalculation(){
+    if(controller.isGrounded)
+    {
+        verticalVelocity = -1f;
+        if(Input.GetButtonDown("Jump"))
+        {
+            verticalVelocity = Mathf.Sqrt(jumpHeight * gravity * 2);
+        }
+    }
+    else
+    {
+        verticalVelocity -= gravity * Time.deltaTime;
+    }
+
+    return verticalVelocity;
+    }
+    
+    
 }
