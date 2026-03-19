36b53ad4f02ff86eed8df45be2ef32f0b2b5714e
DylanFoon <foondylan@gmail.com>
2026-03-19 00:33:18 -0400

    Inital MainHub Commit

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,35 +2,46 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    
-    public float speed = 5f;
-    
-    public float _mouseSensitivity = 2f; 
+    public float baseSpeed = 5f;
+
+    public float _mouseSensitivity = 2f;
+
     private Vector3 moveInput;
+
     private float rotationY;
-    
-    // Update is called once per frame
+
+    private float defaultScale;
+
+    void Start()
+    {
+        defaultScale = transform.localScale.x;
+    }
+
     void Update()
     {
         HandleMovement();
+        HandleRotation();
     }
 
     private void HandleMovement()
-    {
-        float horizontal = Input.GetAxis("Horizontal");
+    {        float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-        
+
         moveInput = new Vector3(horizontal, 0f, vertical).normalized;
-        
+
+        //Scale speed proportionally to the character's current size.
+        float scaleMultiplier = transform.localScale.x / defaultScale;
+        float speed = baseSpeed * scaleMultiplier;
+
         transform.Translate(moveInput * speed * Time.deltaTime);
     }
 
     private void HandleRotation()
     {
         float mouseX = Input.GetAxis("Mouse X");
-        rotationY +=  mouseX * _mouseSensitivity;
-        
+
+        rotationY += mouseX * _mouseSensitivity;
+
         transform.rotation = Quaternion.Euler(0f, rotationY, 0f);
     }
-    
 }
