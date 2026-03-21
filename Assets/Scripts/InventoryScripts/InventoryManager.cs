08758720b9b3fe70c0d51b8089c302245f9f6d38
antonynguyen2005-lgtm <antony.nguyen2005@gmail.com>
2026-03-20 23:57:32 -0400

    Added more stuff to the inventory
    Added the collection method selection, and description Also added stuff to make things into items which you can collect

---
+++ Assets/Scripts/InventoryScripts/InventoryManager.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    public GameObject InventoryMenu;
+    private bool menuActivated;
+    public ItemSlot[] itemSlot;
+
+    public bool MenuActivated
+    {
+        get { return menuActivated; }
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Inventory") && menuActivated)
+        {
+            
+            InventoryMenu.SetActive(false);
+            menuActivated = false;
+
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+        else if (Input.GetButtonDown("Inventory") && !menuActivated)
+        {
+           
+            InventoryMenu.SetActive(true);
+            menuActivated = true;
+
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+
+    public void AddItem(string itemName, int quantity, Sprite itemSprite, string itemDescription)
+    {
+        for (int i = 0; i < itemSlot.Length; i++)
+        {
+            if (itemSlot[i].isFull && itemSlot[i].itemName == itemName)
+            {
+                itemSlot[i].AddQuantity(quantity);
+                return;
+            }
+        }
+
+        for (int i = 0; i < itemSlot.Length; i++)
+        {
+            if (!itemSlot[i].isFull)
+            {
+                itemSlot[i].AddItem(itemName, quantity, itemSprite,itemDescription);
+                return;
+            }
+        }
+    }
+
+    public void DeselectAllSlots()
+    {
+        for (int i = 0; i < itemSlot.Length; i++)
+        {
+            if (itemSlot[i] != null)
+            {
+                itemSlot[i].thisItemSelected = false;
+
+                if (itemSlot[i].selectedShader != null)
+                {
+                    itemSlot[i].selectedShader.SetActive(false);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/InventoryScripts/ItemSlot.cs
@@ -0,0 +1,92 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ItemSlot : MonoBehaviour, IPointerClickHandler
+{
+    public string itemName;
+    public int quantity;
+    public Sprite itemSprite;
+    public bool isFull;
+    public string itemDescription;
+
+    public GameObject selectedShader;
+    public bool thisItemSelected;
+
+    [SerializeField] private TMP_Text quantityText;
+    [SerializeField] private Image itemImage;
+
+    private InventoryManager inventoryManager;
+
+    public Image ItemDescriptionImage;
+    public TMP_Text ItemDescriptionNameText;
+    public TMP_Text ItemDescriptionText;
+
+    private void Start()
+    {
+        inventoryManager = FindFirstObjectByType<InventoryManager>();
+
+        if (selectedShader != null)
+            selectedShader.SetActive(false);
+    }
+
+    public void AddItem(string itemName, int quantity, Sprite itemSprite,string itemDescription)
+    {
+        this.itemName = itemName;
+        this.quantity = quantity;
+        this.itemSprite = itemSprite;
+        this.itemDescription = itemDescription;
+        isFull = true;
+
+        itemImage.sprite = this.itemSprite;
+        itemImage.enabled = true;
+
+        UpdateQuantityText();
+    }
+
+    public void AddQuantity(int amount)
+    {
+        quantity += amount;
+        UpdateQuantityText();
+    }
+
+    private void UpdateQuantityText()
+    {
+        if (quantityText != null)
+        {
+            quantityText.text = quantity.ToString();
+            quantityText.enabled = true;
+        }
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+
+        if (eventData.button == PointerEventData.InputButton.Left)
+        {
+            OnLeftClick();
+        }
+
+        if (eventData.button == PointerEventData.InputButton.Right)
+        {
+            OnRightClick();
+        }
+    }
+
+
+    public void OnLeftClick()
+        {
+            inventoryManager.DeselectAllSlots();
+            selectedShader.SetActive(true);
+            thisItemSelected = true;
+            ItemDescriptionNameText.text = itemName;
+            ItemDescriptionText.text = itemDescription;
+            ItemDescriptionImage.sprite = itemSprite;
+        }
+    
+    public void OnRightClick(){
+
+
+    }
+}
